c481e16e68e3fd9235ccebaf084bf4b7924e42ac
Simon Dutil <sdutil@lcs.on.ca>
2019-03-28 09:20:40 -0400

    got person Jim there

---
+++ MyGame/Jim.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Jim here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jim extends Actor
+{
+    /**
+     * Act - do whatever the Jim wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
